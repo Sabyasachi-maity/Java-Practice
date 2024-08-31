c9b4f69f2f4f05153b785db24ad33688d0d808c6
Sabyasachi Maity <sabyasachimaity2000@gmail.com>
2024-08-31 23:58:22 +0530

    minor

---
+++ LOOP/loop.java
@@ -0,0 +1,15 @@
+package LOOP;
+
+public class loop 
+    
+        public static void main(String[]args)
+         int i=0;
+         while(i<=5){
+             if (i==3){
+                 i++;
+                 continue;
+             }
+             System.out.println(i);
+            i++;
+    
+}
